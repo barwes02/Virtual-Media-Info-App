aba4a09b90a121fd38c191ebed99f6f8d70c59b4
sam Barwell-smith <sbarwellsmith@gmail.com>
2020-09-13 00:45:52 +1200

    update

--- config/livewire.php
+++ config/livewire.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'asset_url'  => 'http://vmi-app.herokuapp.com/',
+    'asset_url'  => 'http://vmi-app.herokuapp.com',
 
     /*
     |--------------------------------------------------------------------------
